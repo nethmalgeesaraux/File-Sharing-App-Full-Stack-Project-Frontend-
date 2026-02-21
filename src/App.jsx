22e94be047df85dff24ad3dffd643a485c0738bd
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-02-21 21:31:21 +0530

    add file structure & data/asset.js files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import MyFiles from './pages/MyFiles'
+import Subscription from './pages/Subscription'
+import Transactions from './pages/Transactions'
+import Upload from './pages/Upload'
+
 const App = () => {
-  return(
-    <div>App Component</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/my-files" element={<MyFiles />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/transactions" element={<Transactions/>} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
